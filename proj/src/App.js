685f44d5a0b0ef8ef0c9acedca8ffd7f95b0bef2
Runwei-Bethelihem <Intern.Bethelihem@runwei.org>
2024-06-10 11:38:48 -0400

    FEAT:Installed React

---
+++ proj/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <t1>HI Thank you</t1>
+    </div>
+  );
+}
+
+export default App;
